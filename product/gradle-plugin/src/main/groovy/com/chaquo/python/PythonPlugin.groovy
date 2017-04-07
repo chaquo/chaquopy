c4e87e3ee56d68cb2adc1750e1cf4fbbdbb54f9b
Malcolm Smith <smith@xanadu>
2017-04-07 16:11:41 +0100

    Package assets and jniLibs from target Python directory

--- product/gradle-plugin/src/main/groovy/com/chaquo/python/PythonPlugin.groovy
+++ product/gradle-plugin/src/main/groovy/com/chaquo/python/PythonPlugin.groovy
@@ -1,16 +1,18 @@
 package com.chaquo.python
 
 import org.gradle.api.*
-import org.gradle.api.plugins.ExtensionAware
+import org.gradle.api.plugins.*
+import org.gradle.api.tasks.*
 
 // FIXME unit test everything
 class PythonPlugin implements Plugin<Project> {
     public static final String NAME = "python"
 
+    Project project
     def android
-    File genDir, intDir
 
     void apply(Project project) {
+        this.project = project
         android = project.android
 
         extend(android.defaultConfig)
@@ -19,14 +21,14 @@ class PythonPlugin implements Plugin<Project> {
 
         // TODO add "python" source set property
 
-        project.afterEvaluate { afterEvaluate(it) }
+        project.afterEvaluate { afterEvaluate() }
     }
 
     void extend(ExtensionAware ea) {
         ea.extensions.create(NAME, PythonExtension)
     }
 
-    void afterEvaluate(Project project) {
+    void afterEvaluate() {
         for (variant in android.applicationVariants) {
             def python = new PythonExtension()
             python.mergeFrom(android.defaultConfig)
@@ -38,10 +40,8 @@ class PythonPlugin implements Plugin<Project> {
                                           "You may want to add it to defaultConfig.")
             }
 
-            genDir = new File(project.buildDir, "generated")
-            intDir = new File(project.buildDir, "intermediates")
-            createAssetTask(project, variant, python);
-            // TODO native libs
+            createAssetTask(variant, python);
+            createNativeLibsTask(variant, python)
             // TODO Java lib (maybe better to generate Gradle dependency so it's on
             // the classpath for interactive editing)
             // TODO Java generated source
@@ -49,33 +49,54 @@ class PythonPlugin implements Plugin<Project> {
         }
     }
 
-    void createAssetTask(Project project, variant, PythonExtension python) {
-        File genAssetDir = new File(genDir, "assets/python/$variant.dirName")
-
-        // TODO download Python and packages to non-variant-specific cache in generated/python
-
-        Task genTask = project.task("generatePython${variant.name.capitalize()}Assets") {
-            outputs.dir genAssetDir
-            doLast {
-                project.delete(genAssetDir)
-                project.mkdir(genAssetDir)
-                def pw = new PrintWriter(new File(genAssetDir, "asset.txt"))
-                pw.println("Hello asset")
-                pw.close()
+    void createAssetTask(variant, PythonExtension python) {
+        File assetDir = genVariantDir(variant, "assets")
+        Task genTask = project.task("generatePython${variant.name.capitalize()}Assets",
+                                    type: Copy) {
+            doFirst {
+                project.delete(assetDir)
             }
+            from project.file("${targetDir(python)}/stdlib.zip", PathValidation.FILE)
+            into assetDir
         }
+        extendMergeTask(variant.getMergeAssets(), genTask, genTask.outputs)
+    }
 
-        Task mergeTask = variant.getMergeAssets()
-        mergeTask.dependsOn(genTask)
-        mergeTask.inputs.dir(genAssetDir)
-        mergeTask.doLast {
-            project.copy {
-                from genAssetDir
-                into mergeTask.outputDir
+    void createNativeLibsTask(variant, PythonExtension python) {
+        for (mergeTask in
+             project.getTasksByName("merge${variant.name.capitalize()}JniLibFolders", false)) {
+            // TODO use ABI filters
+            extendMergeTask(mergeTask, null,
+                            project.file("${targetDir(python)}/lib", PathValidation.DIRECTORY))
+        }
+    }
+
+    void extendMergeTask(Task mergeTask, depTasks, files) {
+        if (depTasks) {
+            mergeTask.dependsOn(depTasks)
+        }
+        if (files) {
+            mergeTask.inputs.files(files)
+            mergeTask.doLast {
+                project.copy {
+                    from files
+                    into mergeTask.outputDir
+                }
             }
         }
     }
 
+    private File genDir() {
+        return new File(project.buildDir, "generated/${NAME}")
+    }
+
+    File targetDir(PythonExtension python) {
+        return new File(genDir(), "target/$python.version")
+    }
+
+    private File genVariantDir(variant, String type) {
+        return new File(genDir(), "$type/$variant.dirName")
+    }
 }
 
 
