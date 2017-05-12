0a11fc202610eda7650108d0c967883e72c8cfb1
Malcolm Smith <smith@xanadu>
2017-05-12 01:27:29 +0100

    Package runtime components into gradle-plugin

---
+++ product/buildSrc/src/main/java/com/chaquo/python/Common.java
@@ -0,0 +1,12 @@
+package com.chaquo.python;
+
+
+import java.util.*;
+
+public class Common {
+    public static final List<String> PYTHON_VERSIONS = Arrays.asList
+        ("2.7.10");                                                     // TODO #5148
+    public static final List<String> ABIS = Arrays.asList
+        ("x86", "x86_64", "armeabi", "armeabi-v7a", "arm64-v8a");
+    public static final String ASSET_DIR = "chaquopy";
+}
