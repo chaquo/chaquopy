6d4c660de32d23d3a4ce9e08a7099bb01a7f9b2d
Malcolm Smith <smith@xanadu>
2017-09-05 12:57:47 +0100

    Make consoleScript task output to a file rather than stdout

--- product/runtime/shell.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-export CLASSPATH='C:\Users\smith\cygwin\git\chaquo\python\product\runtime\build\classes\main;C:\Users\smith\cygwin\git\chaquo\python\product\runtime\build\classes\test'
-export PYTHONPATH='C:\Users\smith\cygwin\git\chaquo\python\product\runtime/src/main/python;C:\Users\smith\cygwin\git\chaquo\python\product\runtime/src/test/python'
-export PATH="C:\Users\smith\cygwin\git\chaquo\python\product\runtime\build/cmake/2.7/host;$PATH"
-winpty python2.7.exe
