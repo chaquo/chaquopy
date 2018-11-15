5f5be28feddc86f4e41821e685f501ef661cd157
Malcolm Smith <smith@chaquo.com>
2018-11-15 20:37:11 +0000

    Update Docker build process

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 set -eu
 
-mkdir -p maven
-
-echo "# gradle"
-gradle_dir=maven/com/chaquo/python/gradle/$(cat VERSION.txt)
-mkdir -p $gradle_dir
-cp product/gradle-plugin/build/libs/* $gradle_dir
-
-echo "# target"
 target_dir=maven/com/chaquo/python/target
 common_java="product/buildSrc/src/main/java/com/chaquo/python/Common.java"
-for major_minor in 2.7 3.6; do
-    echo "## $major_minor"
-    line=$(grep "PYTHON_BUILD_NUMBERS.put(\"$major_minor" $common_java | tail -n1)
-    micro=$(echo $line | sed -E "s/.*\"$major_minor.([0-9]+).*/\1/")
-    build=$(echo $line | sed -E "s/.*, \"([0-9]+).*/\1/")
-    ./target/package-target.sh crystax $major_minor $micro-$build $target_dir
-done
+extract_string='s/.*"(.*)".*/\1/'
+python_version=$(grep "PYTHON_VERSION =" $common_java | sed -E "$extract_string")
+build=$(grep "PYTHON_BUILD_NUM =" $common_java | sed -E "$extract_string")
+major_minor=$(echo $python_version | sed -E 's/(.*)\..*/\1/')
+micro=$(echo $python_version | sed -E 's/.*\.(.*)/\1/')
+./target/package-target.sh crystax $major_minor $micro-$build $target_dir
--- product/buildSrc/src/main/java/com/chaquo/python/Common.java
+++ product/buildSrc/src/main/java/com/chaquo/python/Common.java
@@ -15,7 +15,7 @@ public class Common {
     public static final String PYTHON_VERSION = "3.6.5";
     public static final String PYTHON_VERSION_SHORT =
         PYTHON_VERSION.substring(0, PYTHON_VERSION.lastIndexOf('.'));
-    public static final String PYTHON_BUILD_NUM = "8";
+    public static final String PYTHON_BUILD_NUM = "9";
 
     // Library name suffix: may contain flags from PEP 3149.
     public static final String PYTHON_SUFFIX = PYTHON_VERSION_SHORT + "m";
