92b2515393d42ecef16b1b34fe7b742f8c3fd2b2
Malcolm Smith <smith@chaquo.com>
2019-01-06 22:21:13 +0000

    Add package ephem

---
+++ server/pypi/packages/ephem/test.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import, division, print_function
+
+import unittest
+
+
+class TestEphem(unittest.TestCase):
+
+    def test_basic(self):
+        import ephem
+        u = ephem.Uranus()
+        u.compute("1781/3/13")
+        self.assertEqual("5:35:45.28", str(u.ra))
