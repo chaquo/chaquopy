b6cd9d2fd69861558422b6c228475e72f15e95af
Malcolm Smith <smith@chaquo.com>
2019-04-02 14:54:39 +0000

    Add proper test for kiwisolver

--- server/pypi/packages/kiwisolver/test.py
+++ server/pypi/packages/kiwisolver/test.py
@@ -1,10 +1,25 @@
-from __future__ import absolute_import, division, print_function
-
 import unittest
 
 
 class TestKiwisolver(unittest.TestCase):
 
-    # Minimal import test: we only added this package because it's a dependency of matplotlib.
+    # From https://kiwisolver.readthedocs.io/en/latest/basis/basic_systems.html
     def test_basic(self):
-        import kiwisolver  # noqa: F401
+        from kiwisolver import Solver, Variable
+
+        x1 = Variable('x1')
+        x2 = Variable('x2')
+        xm = Variable('xm')
+
+        constraints = [x1 >= 0, x2 <= 100, x2 >= x1 + 10, xm == (x1 + x2) / 2]
+        solver = Solver()
+        for cn in constraints:
+            solver.addConstraint(cn)
+
+        solver.addConstraint((x1 == 40) | "weak")
+        solver.addEditVariable(xm, 'strong')
+        solver.suggestValue(xm, 60)
+        solver.updateVariables()
+        self.assertEqual(60, xm.value())
+        self.assertEqual(40, x1.value())
+        self.assertEqual(80, x2.value())
