743d789ac9233402c711dbc64ca0acbe7a59dc8f
Malcolm Smith <smith@chaquo.com>
2019-06-28 14:47:53 +0000

    Update package: scikit-learn

--- server/pypi/packages/numpy/test.py
+++ server/pypi/packages/numpy/test.py
@@ -8,3 +8,15 @@ class TestNumpy(unittest.TestCase):
     def test_basic(self):
         from numpy import array
         self.assertEqual([4, 7], (array([1, 2]) + array([3, 5])).tolist())
+
+    # This will probably fail if optimized BLAS is not being used.
+    def test_performance(self):
+        import numpy as np
+        from time import time
+
+        start_time = time()
+        SIZE = 500
+        a = np.random.rand(SIZE, SIZE)
+        b = np.random.rand(SIZE, SIZE)
+        np.dot(a, b)
+        self.assertLess(time() - start_time, 1)
