f3335165e51368ba260a7e5e1e98611969ede55d
Malcolm Smith <smith@chaquo.com>
2021-09-15 11:55:20 +0000

    Update package: tflite-runtime

--- server/pypi/packages/tflite-runtime/build.sh
+++ server/pypi/packages/tflite-runtime/build.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 set -eu
 
-tensorflow/lite/tools/pip_package/build_pip_package.sh
-build_dir="/tmp/tflite_pip"
-unzip -q -d ../prefix $build_dir/*/dist/*.whl
-rm -r $build_dir
+# Values were determined by inspection of the Makefile.
+export TENSORFLOW_TARGET="linux"
+case $CHAQUOPY_ABI in
+    armeabi-v7a)
+        export TENSORFLOW_TARGET_ARCH="armv7l"
+        ;;
+    arm64-v8a)
+        export TENSORFLOW_TARGET_ARCH="aarch64"
+        ;;
+    x86)
+        export TENSORFLOW_TARGET_ARCH="x86_32"
+        ;;
+    x86_64)
+        export TENSORFLOW_TARGET_ARCH="x86_64"
+        ;;
+    *)
+        echo "Unknown ABI '$CHAQUOPY_ABI'"
+        exit 1
+        ;;
+esac
+
+pp_dir="tensorflow/lite/tools/pip_package"
+$pp_dir/build_pip_package.sh
+unzip -q -d ../prefix $pp_dir/gen/tflite_pip/python3/dist/*.whl
