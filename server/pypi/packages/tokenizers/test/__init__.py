738617ef70bd9b4b0f72ff18ce09a56ba7e0975d
Malcolm Smith <smith@chaquo.com>
2022-01-17 20:10:08 +0000

    Update package: tokenizers

--- server/pypi/packages/tokenizers/test/__init__.py
+++ server/pypi/packages/tokenizers/test/__init__.py
@@ -13,11 +13,12 @@ class TestTokenizers(unittest.TestCase):
 
         for size, vocab, tokens in [
             (0, "? a c d f h i k l m o u w", "c h i c k"),
-            (20, "? a c ch chuck ck d f h i k l m o od u uck w wo wood", "ch i ck")]:
+            (20, "? a c ch chuck ck d f h i k l m o od u uck w wo wood", "ch i ck"),
+        ]:
             with self.subTest(size=size):
                 tokenizer = Tokenizer(BPE())
                 tokenizer.pre_tokenizer = Whitespace()
                 trainer = BpeTrainer(vocab_size=size)
-                tokenizer.train(trainer, [f"{dirname(__file__)}/train.txt"])
+                tokenizer.train([f"{dirname(__file__)}/train.txt"], trainer)
                 self.assertEqual(vocab.split(), sorted(tokenizer.get_vocab()))
                 self.assertEqual(tokens.split(), tokenizer.encode("chick").tokens)
--- target/unpackage-target.sh
+++ target/unpackage-target.sh
@@ -10,14 +10,18 @@ set -eu
 target_dir="$(realpath ${1:?})"
 toolchains_dir="$(realpath "${2:?}")"
 
+version=$(basename "$target_dir")
+version_short=$(echo $version | sed -E 's/^([0-9]+\.[0-9]+).*/\1/')
+
 tmp_dir="/tmp/unpackage-target-$$"
 mkdir "$tmp_dir"
 
 cd "$toolchains_dir"
 for abi in *; do
+    echo "$abi"
     abi_dir="$tmp_dir/$abi"
     mkdir "$abi_dir"
-    unzip -q -d "$abi_dir" "$target_dir/target-"*"-$abi.zip"
+    unzip -q -d "$abi_dir" "$target_dir/target-$version-$abi.zip"
 
     prefix="$toolchains_dir/$abi/sysroot/usr"
     mkdir -p "$prefix/include"
@@ -28,6 +32,14 @@ for abi in *; do
     for name in "$prefix/lib/"*_chaquopy.so; do
         ln -sf "$(basename "$name")" "$(echo $name | sed 's/_chaquopy//')"
     done
+
+    # TODO: this gives every ABI the _sysconfigdata module from x86_64. This causes problems
+    # with Rust packages such as tokenizers, which access that module at build time
+    # (https://github.com/PyO3/pyo3/blob/v0.12.4/build.rs).
+    lib_python="$prefix/lib/python$version_short"
+    rm -rf "$lib_python"
+    mkdir -p "$lib_python"
+    unzip -q -d "$lib_python" "$target_dir/target-$version-stdlib.zip"
 done
 
 rm -rf "$tmp_dir"
