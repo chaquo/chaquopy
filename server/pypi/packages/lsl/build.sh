f269888326979bbc0b2e65ecdca855a55fd1074d
KyucraftV2 <killian.reiner@etu.umontpellier.fr>
2023-05-12 10:26:34 +0200

    Modif sur le build et le meta

--- server/pypi/packages/lsl/build.sh
+++ server/pypi/packages/lsl/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -eu
 
-git clone --depth=1 https://github.com/sccn/liblsl.git
 cmake .. -DCMAKE_TOOLCHAIN_FILE="$SRC_DIR/../chaquopy.toolchain.cmake" \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
       -DBUILD_TESTING=OFF
