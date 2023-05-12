9904968b98c2f3b8f90ba4885c25801bb1599637
KyucraftV2 <killian.reiner@etu.umontpellier.fr>
2023-05-12 10:27:47 +0200

    Modif

--- server/pypi/packages/lsl/build.sh
+++ server/pypi/packages/lsl/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eu
 
+git clone --depth=1 https://github.com/sccn/liblsl.git
 cmake .. -DCMAKE_TOOLCHAIN_FILE="$SRC_DIR/../chaquopy.toolchain.cmake" \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
       -DBUILD_TESTING=OFF
