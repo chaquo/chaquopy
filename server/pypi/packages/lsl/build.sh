a7f36d73d70bcf95b2bb398cb01a703aea5d7f3c
root <root@debian11.home.local>
2023-05-12 10:23:56 +0200

    Modif du build

--- server/pypi/packages/lsl/build.sh
+++ server/pypi/packages/lsl/build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -eu
 
-mkdir build
-cd build
 git clone --depth=1 https://github.com/sccn/liblsl.git
-mkdir build
-cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE="$SRC_DIR/../chaquopy.toolchain.cmake" \
       -DCMAKE_INSTALL_PREFIX="$PREFIX" \
       -DBUILD_TESTING=OFF
