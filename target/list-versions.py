5ac45dad26cb0a185c335a101f8e6d82efda0d85
Malcolm Smith <smith@chaquo.com>
2023-07-19 23:11:33 +0100

    Add test-integration job

--- target/list-versions.py
+++ target/list-versions.py
@@ -6,19 +6,29 @@ import re
 
 parser = argparse.ArgumentParser()
 mode_group = parser.add_mutually_exclusive_group(required=True)
+mode_group.add_argument("--default", action="store_true")
 mode_group.add_argument("--short", action="store_true")
 mode_group.add_argument("--long", action="store_true")
 args = parser.parse_args()
 
 product_dir = abspath(f"{dirname(__file__)}/../product")
+lines = []
 for line in open(f"{product_dir}/buildSrc/src/main/java/com/chaquo/python/Common.java"):
-    match = re.search(r'PYTHON_VERSIONS.put\("(\d+)\.(\d+)\.(\d+)", "(\d+)"\)', line)
-    if match:
-        major, minor, micro, build = match.groups()
-        if args.short:
-            print(f"{major}.{minor}")
-        elif args.long:
-            print(f"{major}.{minor}.{micro}-{build}")
-        else:
-            raise AssertionError()
+    if args.default:
+        match = re.search(r'DEFAULT_PYTHON_VERSION = "(.+)"', line)
+        if match:
+            lines.append(match[1])
+            break
+    else:
+        match = re.search(r'PYTHON_VERSIONS.put\("(\d+)\.(\d+)\.(\d+)", "(\d+)"\)', line)
+        if match:
+            major, minor, micro, build = match.groups()
+            if args.short:
+                lines.append(f"{major}.{minor}")
+            elif args.long:
+                lines.append(f"{major}.{minor}.{micro}-{build}")
+            else:
+                raise AssertionError()
 
+assert lines
+print("\n".join(lines))
