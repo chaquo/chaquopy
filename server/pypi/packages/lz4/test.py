83f7c2ae52c45b68bccd34a99ff77c05f51c3f8a
Malcolm Smith <smith@chaquo.com>
2023-07-24 14:03:45 +0000

    Fix test

--- server/pypi/packages/lz4/test.py
+++ server/pypi/packages/lz4/test.py
@@ -8,6 +8,6 @@ class TestLz4(unittest.TestCase):
         import lz4.frame
         input_data = 20 * 128 * os.urandom(1024)
         compressed = lz4.frame.compress(input_data)
+        self.assertLess(len(compressed), len(input_data))
         decompressed = lz4.frame.decompress(compressed)
-        unittest.assertEqual(decompressed, input_data)
-
+        self.assertEqual(decompressed, input_data)
