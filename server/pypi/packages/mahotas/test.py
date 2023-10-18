da64b722eb92a154ac7e156d2baf8db1d41e6f83
IsakTheHacker <67378443+IsakTheHacker@users.noreply.github.com>
2023-10-18 21:48:22 +0200

    Write test for mahotas (currenly failing)

---
+++ server/pypi/packages/mahotas/test.py
@@ -0,0 +1,22 @@
+import unittest
+
+class TestMahotas(unittest.TestCase):
+
+    def test_hitmiss(self):
+        import mahotas
+        import numpy as np
+
+        A = np.zeros((100,100), np.bool_)
+        Bc = np.array([
+            [0,1,2],
+            [0,1,1],
+            [2,1,1]])
+        mahotas.morph.hitmiss(A,Bc)
+        self.assertEqual(mahotas.morph.hitmiss(A,Bc).sum(), False)
+
+        A[4:7,4:7] = np.array([
+            [0,1,1],
+            [0,1,1],
+            [0,1,1]])
+        self.assertEqual(mahotas.morph.hitmiss(A,Bc).sum(), 1)
+        self.assertEqual(mahotas.morph.hitmiss(A,Bc)[5,5], True)
