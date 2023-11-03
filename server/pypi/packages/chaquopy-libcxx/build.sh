36ca15799598a0ff3b2136ac50e518e1ec3f72d1
Martin Peterlin <martin.peterlin7@gmail.com>
2023-11-03 01:10:22 +0100

    libc++ - updated build.sh and package version

--- server/pypi/packages/chaquopy-libcxx/build.sh
+++ server/pypi/packages/chaquopy-libcxx/build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -eu
 
-toolchain=$(realpath $(dirname $CC)/..)
+RESOLVED_CC=$($CC -print-prog-name=clang)
+toolchain=$(realpath $(dirname $RESOLVED_CC)/..)
 
-header_version=$(cat $toolchain/sysroot/usr/include/c++/v1/__libcpp_version)
-if [[ $header_version != $PKG_VERSION ]]; then
-    echo "Header version '$header_version' doesn't match meta.yaml version '$PKG_VERSION'"
+actual_version=$(printf "#include <ciso646>\nint main () {}" | $CC -E -stdlib=libc++ -x c++ -dM - | grep -w "#define _LIBCPP_VERSION" | cut -d' ' -f3)
+# actual_version=$(cat $toolchain/sysroot/usr/include/c++/v1/__libcpp_version)
+if [[ $actual_version != $PKG_VERSION ]]; then
+    echo "Actual version '$actual_version' doesn't match meta.yaml version '$PKG_VERSION'"
     exit 1
 fi
 
 mkdir -p $PREFIX/lib
-cp $toolchain/sysroot/usr/lib/$CHAQUOPY_TRIPLET/libc++_shared.so $PREFIX/lib
+cp $toolchain/sysroot/usr/lib/$HOST/libc++_shared.so $PREFIX/lib
