478c654684dee02f73ea960afaf27ff2f334deb0
IsakTheHacker <67378443+IsakTheHacker@users.noreply.github.com>
2023-11-04 21:50:05 +0100

    Write package test for zope.interface

---
+++ server/pypi/packages/zope-interface/test.py
@@ -0,0 +1,24 @@
+import unittest
+import sys
+
+
+class TestZopeInterface(unittest.TestCase):
+
+    def test_inside_function_call(self):
+        from zope.interface.advice import getFrameInfo
+
+        kind, module, f_locals, f_globals = getFrameInfo(sys._getframe())
+        self.assertEqual(kind, "function call")
+        self.assertTrue(f_locals is locals())
+        for d in module.__dict__, f_globals:
+            self.assertTrue(d is globals())
+
+    def test_element(self):
+        from zope.interface.interface import Element
+
+        e1 = Element("foo")
+        e2 = Element("bar")
+        e1.setTaggedValue("x", 1)
+        e2.setTaggedValue("x", 2)
+        self.assertEqual(e1.getTaggedValue("x"), 1)
+        self.assertEqual(e2.getTaggedValue("x"), 2)
