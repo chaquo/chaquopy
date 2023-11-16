63850e240c66fc8a35edd006e74caed34392cc56
Malcolm Smith <smith@chaquo.com>
2023-11-16 08:19:15 +0000

    Update to Android Gradle plugin version 8.1.3

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.1.2" apply false
+    id("com.android.application") version "8.1.3" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.chaquo.python") version System.getProperty("chaquopyVersion") apply false
 }
