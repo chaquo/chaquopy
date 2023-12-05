01c3d08900d613178ba9b1c22751aa039e5363d7
Malcolm Smith <smith@chaquo.com>
2023-12-05 15:43:57 +0000

    Working on Python 3.11: argon2-cffi bitarray editdistance ephem lru-dict pyzbar typed-ast netifaces wordcloud pycurl

--- server/pypi/packages/wordcloud/test/__init__.py
+++ server/pypi/packages/wordcloud/test/__init__.py
@@ -3,22 +3,33 @@ import unittest
 
 class TestWordcloud(unittest.TestCase):
 
-    # See https://github.com/amueller/word_cloud/blob/master/examples/simple.py
+    # https://github.com/amueller/word_cloud/blob/master/examples/simple.py
     def test_basic(self):
         from os.path import dirname, join
+        import numpy as np
         from wordcloud import WordCloud
 
         with open(join(dirname(__file__), "constitution.txt")) as text_file:
             text = text_file.read()
         wordcloud = WordCloud(random_state=42).generate(text)
-        a = wordcloud.to_array()
-        self.assertEqual((200, 400, 3), a.shape)
 
-        for x, y, color in [(144, 123, (72, 32, 113)),  # Purple ("State")
-                            (265, 127, (72, 32, 113)),
-                            (124, 60, (129, 211, 77)),  # Green ("Congress")
-                            (218, 66, (129, 211, 77)),
-                            (155, 63, (0, 0, 0)),       # Black (background)
-                            (144, 137, (0, 0, 0))]:
-            with self.subTest(x=x, y=y):
-                self.assertEqual(color, tuple(a[y, x]))
+        HEIGHT = 200
+        WIDTH = 400
+        a = wordcloud.to_array()
+        self.assertEqual(a.shape, (HEIGHT, WIDTH, 3))
+        self.assertEqual(a.dtype, np.uint8)
+
+        # Output is not perfectly reproducible, even with a fixed random_state. And even
+        # wordcloud's own unit tests don't appear to cover the layout algorithm. So just
+        # make sure that the middle row of the image contains at least one black pixel
+        # and one reasonably-light pixel.
+        found_black = found_light = False
+        for x in range(WIDTH):
+            color = a[HEIGHT // 2, x]
+            if tuple(color) == (0, 0, 0):
+                found_black = True
+            elif max(color) > 100:
+                found_light = True
+
+        self.assertTrue(found_black)
+        self.assertTrue(found_light)
