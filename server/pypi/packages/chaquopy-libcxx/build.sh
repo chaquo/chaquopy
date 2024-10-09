a21a3d7950b89e288472f8814b3e796637279471
Malcolm Smith <smith@chaquo.com>
2024-10-09 16:30:39 +0000

    Update chaquopy-libcxx

--- server/pypi/packages/chaquopy-libcxx/build.sh
+++ server/pypi/packages/chaquopy-libcxx/build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-set -eu
+set -eu -o pipefail
 
-toolchain=$(realpath $(dirname $CC)/..)
+toolchain=$(realpath $(dirname $AR)/..)
 
-header_version=$(cat $toolchain/sysroot/usr/include/c++/v1/__libcpp_version)
-if [[ $header_version != $PKG_VERSION ]]; then
+pattern='^ *# *define +_LIBCPP_VERSION +([0-9]+)$'
+header_version=$(
+    grep -E "$pattern" "$toolchain/sysroot/usr/include/c++/v1/__config" |
+    sed -E "s/$pattern/\1/"
+)
+if [ "$header_version" != "$PKG_VERSION" ]; then
     echo "Header version '$header_version' doesn't match meta.yaml version '$PKG_VERSION'"
     exit 1
 fi
 
 mkdir -p $PREFIX/lib
-cp $toolchain/sysroot/usr/lib/$CHAQUOPY_TRIPLET/libc++_shared.so $PREFIX/lib
+cp $toolchain/sysroot/usr/lib/$HOST/libc++_shared.so $PREFIX/lib
