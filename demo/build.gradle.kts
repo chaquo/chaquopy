7ceffeba6c036ea67e745d969ad30fcbbca255f4
Malcolm Smith <smith@chaquo.com>
2024-11-05 23:04:51 +0000

    Update to Android Gradle plugin version 8.7.2

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all
 // sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.7.1" apply false
+    id("com.android.application") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 
     // com.chaquo.python is declared in settings.gradle.kts, because dynamic versions
