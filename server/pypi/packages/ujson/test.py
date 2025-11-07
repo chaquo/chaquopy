ea141d0ce277a9bbba064e9b9c4de1ed48a17ff8
Malcolm Smith <smith@chaquo.com>
2025-11-07 13:06:09 +0000

    Working on Python 3.10: blis gensim google-crc32c marisa-trie opencv-contrib-python opencv-contrib-python-headless opencv-python opencv-python-headless psutil pycares pyzmq sentencepiece soundfile spacy statsmodels thinc ujson

--- server/pypi/packages/ujson/test.py
+++ server/pypi/packages/ujson/test.py
@@ -6,5 +6,11 @@ class TestUjson(unittest.TestCase):
 
     def test_basic(self):
         import ujson
-        self.assertEqual("3.1415926536", ujson.dumps(math.pi))
-        self.assertEqual("3.142", ujson.dumps(math.pi, double_precision=3))
+        self.assertEqual(ujson.dumps(math.pi), "3.141592653589793")
+
+        script = "<script>John&Doe"
+        self.assertEqual(ujson.dumps(script), f'"{script}"')
+        self.assertEqual(
+            ujson.dumps(script, encode_html_chars=True),
+            '"\\u003cscript\\u003eJohn\\u0026Doe"'
+        )
